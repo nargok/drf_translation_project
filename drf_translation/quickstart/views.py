b4689c4b606a2c307f17319cf986465fc731df52
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2019-04-05 21:48:35 +0900

    Added UserViewSet, GroupViewSet

--- drf_translation/quickstart/views.py
+++ drf_translation/quickstart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from drf_translation.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+  """
+  ユーザの参照や編集をさせるAPIのエンドポイント
+  """
+  queryset = User.objects.all().order_by('-date_joined')
+  serialzer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+  """
+  グループの参照や編集をさせるAPIのエンドポイント
+  """
+  queryset = Group.objects.all()
+  serializer_class = GroupSerializer
